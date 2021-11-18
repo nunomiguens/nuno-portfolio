a5989f20852ac9e9f585a89cd75a77f1232f2cc1
nunomiguens <nunomiguens@gmail.com>
2021-11-18 11:40:05 +0000

    update nav

---
+++ README.md
@@ -0,0 +1,24 @@
+# Portfolio Website
+
+> A beautiful minimal and accessible website used for my portfolio.
+
+- Deployed Version : #
+
+## Technologies
+
+1. HTML
+2. CSS
+3. Javascript
+
+## Features
+
+- Clean, simple and modern UI design
+- Uses no CSS or JavaScript Frameworks or libraries as dependencies
+- Fully Responsive
+- Mobile navigation menu
+- Switch dark / light theme
+- Contact form fully functional when deployed on netlify
+
+## Usage
+
+Clone it and use it!
---
+++ index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+/* ----- Back to Top Button ----- */
+
+const backToTopButton = document.querySelector('.back-to-top');
+let isBackToTopRendered = false;
+
+let alterStyles = isBackToTopRendered => {
+  backToTopButton.style.visibility = isBackToTopRendered ? 'visible' : 'hidden';
+  backToTopButton.style.opacity = isBackToTopRendered ? 1 : 0;
+  backToTopButton.style.transform = isBackToTopRendered
+    ? 'scale(1)'
+    : 'scale(0)';
+};
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 700) {
+    isBackToTopRendered = true;
+    alterStyles(isBackToTopRendered);
+  } else {
+    isBackToTopRendered = false;
+    alterStyles(isBackToTopRendered);
+  }
+});
+
+/* ----- Submit outside form ----- */
+
+function myFunction() {
+  document.getElementById('form1').submit();
+}
+
+/* ----- Toggle dark mode ----- */
+
+function darkMode() {
+  const element = document.body;
+  element.classList.toggle('dark-mode');
+}
+
+/* ----- Toggle Nav menu ----- */
+
+const toggle = document.querySelector('.toggle');
+const navigation = document.querySelector('.nav__items');
+
+toggle.addEventListener('click', () => {
+  toggle.classList.toggle('active');
+  navigation.classList.toggle('active');
+});
+
+navigation.addEventListener('click', () => {
+  toggle.classList.toggle('active');
+  navigation.classList.toggle('active');
+});
+
+
